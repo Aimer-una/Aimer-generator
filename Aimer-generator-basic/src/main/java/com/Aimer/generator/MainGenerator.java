68c07970142352baf46b7f59b149661a64aa242d
chenxingyong <2103095120@qq.com>
2025-03-31 19:49:09 +0800

    第一阶段生成动态文件和静态文件

---
+++ Aimer-generator-basic/src/main/java/com/Aimer/generator/MainGenerator.java
@@ -0,0 +1,32 @@
+package com.Aimer.generator;
+
+import com.Aimer.model.MainTemplateConfig;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainGenerator {
+    public static void main(String[] args) throws TemplateException, IOException {
+
+        //1.静态文件生成
+        String propertyPath = System.getProperty("user.dir");
+        //输入路径
+        String inputPath = propertyPath + File.separator + "Aimer-generator-demo-project" + File.separator + "acm-template";
+        //输出路径
+        String outPath = propertyPath;
+        //复制
+        StaticGenerator.copyFilesByRecursive(inputPath,outPath);
+
+        //2.动态文件生成
+        String dynamicInputPath = propertyPath + File.separator + "Aimer-generator-basic" + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String dynamicOutputPath = propertyPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
+
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("Aimer");
+        mainTemplateConfig.setLoop(true);
+        mainTemplateConfig.setOutputText("求和结果：");
+        DynamicGenerator.doGenerate(dynamicInputPath, dynamicOutputPath, mainTemplateConfig);
+
+    }
+}
